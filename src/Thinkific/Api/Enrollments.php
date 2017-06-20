8519deddb2cc55f9b983759b0925f8bd2240368e
Kyle Yee <kanasite@users.noreply.github.com>
2017-06-21 00:01:53 +0800

    Update Enrollments.php

--- src/Thinkific/Api/Enrollments.php
+++ src/Thinkific/Api/Enrollments.php
@@ -5,16 +5,18 @@ namespace Thinkific\Api;
 class Enrollments extends AbstractApi {
 
     public function add( $data ) {
+        $class_name = preg_split( '/\\\/', get_class( $this ) );
         return json_decode( $this->client->request( [
-            "endpoint"   => strtolower( array_pop( preg_split( '/\\\/', get_class( $this ) ) ) ),
+            "endpoint"   => strtolower( array_pop( $class_name ) ),
             "httpmethod" => "POST",
             "query"      => $data
         ] ), true );
     }
 
     public function query( $data ) {
+        $class_name = preg_split( '/\\\/', get_class( $this ) );
         return json_decode( $request = $this->client->request( [
-            "endpoint"   => strtolower( $tmparr = array_pop( $tmp = preg_split( '/\\\/', get_class( $this ) ) ) ),
+            "endpoint"   => strtolower( array_pop( $class_name ) ),
             "httpmethod" => "GET",
             "query"      => $data
         ] ), true );
